ab5f0aac057a1364026d17fb9e9400cf9c4853ba
dezlymacauley <dezlymacauley@gmail.com>
2026-09-13 22:17:25 +0200

    C lab revamp

---
+++ d02-core-syntax/d01-single-file-programs/programs/d01-topic-one/f02_bravo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("\nThis is f02_bravo.c\n\n");
+    return 0;
+}
---
+++ d02-core-syntax/d01-single-file-programs/programs/d02-topic-two/f01_charlie.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("\nThis is f01_charlie.c\n\n");
+    return 0;
+}
