267383d4ac423a4d63829c6a4b9afc0ae02e5057
dezlymacauley <dezlymacauley@gmail.com>
2026-09-23 20:07:30 +0200

    Added guide on fixed sized arrays

---
+++ d02-core-syntax/d01-single-file-programs/programs/d02-aggregate-data-types/f01_fixed_sized_arrays.c
@@ -0,0 +1,25 @@
+/*
+    ABOUT: fixed-sized arrays
+
+    An array stores multiple elements contiguously in memory.
+    All of the elements of an array must be of the same data type.
+
+*/
+
+#include <stdio.h>
+int main() {
+
+    int player_scores[4] = {12, 24, 50, 16};
+   
+    printf("\n");
+    printf("The first element is %d\n", player_scores[0]);
+    printf("The second element is %d\n", player_scores[1]);
+    printf("The third element is %d\n", player_scores[2]);
+    printf("The forth element is %d\n", player_scores[3]);
+    printf("\n");
+
+    // The first element is 12
+    // The second element is 24
+    // The third element is 50
+    // The forth element is 16
+}
