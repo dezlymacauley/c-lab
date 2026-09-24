dd16114e0b5cb7c1b52be19c20511bb93e54b0f5
dezlymacauley <dezlymacauley@gmail.com>
2026-09-24 12:21:59 +0200

    Learned how to get the number of elements in a fixed sized array

--- d02-core-syntax/d01-single-file-programs/programs/d03-aggregate-data-types/f01_fixed_sized_arrays.c
+++ d02-core-syntax/d01-single-file-programs/programs/d03-aggregate-data-types/f01_fixed_sized_arrays.c
@@ -6,20 +6,32 @@
 
 */
 
+#include <stddef.h>
 #include <stdio.h>
 int main() {
 
     int player_scores[4] = {12, 24, 50, 16};
-   
+
     printf("\n");
     printf("The first element is %d\n", player_scores[0]);
     printf("The second element is %d\n", player_scores[1]);
     printf("The third element is %d\n", player_scores[2]);
     printf("The fourth element is %d\n", player_scores[3]);
-    printf("\n");
 
     // The first element is 12
     // The second element is 24
     // The third element is 50
     // The fourth element is 16
+
+    //_________________________________________________________________________
+
+    // EXAMPLE: 1 => How to get the number of elements in an fixed-sized array
+
+    size_t number_of_elements_in_player_scores =
+        sizeof(player_scores) / sizeof(player_scores[0]);
+
+    printf("\nnumber_of_elements_in_player_scores: %zu\n\n",
+           number_of_elements_in_player_scores);
+
+    //_________________________________________________________________________
 }
---
+++ d02-core-syntax/d01-single-file-programs/programs/d04-loops/d01_for_loop.c
@@ -0,0 +1,34 @@
+/*
+    ABOUT: `for` loop
+
+    This is used when you want to perform some action for every element 
+    in an aggregate data type like a fixed-sized array.
+
+    The syntax is:
+
+    for (size_t i = index to start at; i < n; i++) {
+        Code that should be run.
+    }
+
+    i is a variable to represent the value of the index.
+
+    n is a variable to represent what index to stop at
+
+    `i++` tells the `for` loop to move to the next element in the list.
+*/
+
+#include <stddef.h>
+#include <stdio.h>
+
+int main(void) {
+
+    // indexes:             0,  1,  2,  3,  4,  5,  6,  7
+    int player_scores[8] = {15, 24, 11, 98, 61, 19, 34, 10};
+
+
+    // for () {
+    //
+    // }
+
+    return 0;
+}
