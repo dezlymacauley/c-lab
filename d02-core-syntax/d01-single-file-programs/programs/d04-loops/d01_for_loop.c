91db9b247dab894211a8807603b8547ac1ea62bc
dezlymacauley <dezlymacauley@gmail.com>
2026-09-24 14:45:07 +0200

    Revised on for loops

--- d02-core-syntax/d01-single-file-programs/programs/d04-loops/d01_for_loop.c
+++ d02-core-syntax/d01-single-file-programs/programs/d04-loops/d01_for_loop.c
@@ -1,20 +1,55 @@
 /*
     ABOUT: `for` loop
 
-    This is used when you want to perform some action for every element 
-    in an aggregate data type like a fixed-sized array.
+    A `for loop` is used as a convinient way to perform an action
+    for every element in a list.
 
-    The syntax is:
+    ___________________________________________________________________________
 
-    for (size_t i = index to start at; i < n; i++) {
-        Code that should be run.
+    This is syntax that makes the most sense in my opinion:
+
+    for (size_t i = first_index_to_iterate; i <= last_index_to_iterate; i++) {
+        Code that should be run
     }
 
-    i is a variable to represent the value of the index.
+    E.g.
 
-    n is a variable to represent what index to stop at
+    for (size_t i = first_index_to_iterate; i <= last_index_to_iterate; i++) {
+        printf("The element at index %zu is %d\n", i, player_scores[i]);
+    }
 
-    `i++` tells the `for` loop to move to the next element in the list.
+    ___________________________________________________________________________
+
+    `size_t i = 0`
+
+    `i` is a variable used for three things:
+    1. To declare what index of the element that the loop should start at
+    2. To represent the current index,
+    as the `for loop` goes through the list.
+    3. To access the value of the element at that index.
+
+    NOTE: You don't have to call this variable `i`
+
+    `i` is just the convention
+    ___________________________________________________________________________
+
+    `i++`
+
+    This means "increase the value of `i` by 1", each time the loop runs,
+    to move to the next element in the list an repeat the same action.
+
+    This is why the data type of `i` is set to `size_t` to ensure that i
+    is large enough the index of the last element in the list.
+    ___________________________________________________________________________
+
+    `i <= last_index_to_iterate`
+
+    The is the condition that is required for the loop to run.
+
+    The loop will run as long as this condition is true.
+
+    ___________________________________________________________________________
+
 */
 
 #include <stddef.h>
@@ -22,13 +57,21 @@
 
 int main(void) {
 
-    // indexes:             0,  1,  2,  3,  4,  5,  6,  7
-    int player_scores[8] = {15, 24, 11, 98, 61, 19, 34, 10};
+    // indexes:             0,  1,  2,  3,  4
+    int player_scores[5] = {15, 24, 11, 98, 52};
 
+    int first_index_to_iterate = 0;
+    int last_index_to_iterate = 4;
 
-    // for () {
-    //
-    // }
+    for (size_t i = first_index_to_iterate; i <= last_index_to_iterate; i++) {
+        printf("The element at index %zu is %d\n", i, player_scores[i]);
+    }
+
+    // The element at index 0 is 15
+    // The element at index 1 is 24
+    // The element at index 2 is 11
+    // The element at index 3 is 98
+    // The element at index 4 is 52
 
     return 0;
 }
