c14852bae1d21390bfcc333fb0b60142ee08a044
dezlymacauley <dezlymacauley@gmail.com>
2026-09-25 10:24:07 +0200

    Updated my knowledge on automatic storage duration

---
+++ d02-core-syntax/d01-single-file-programs/programs/d02-storage-duration/f01_storage_duration_explained.c
@@ -0,0 +1,16 @@
+/*
+    ABOUT: Storage Duration Explained
+
+    In C, storage duration refers to how long an object remains in memory,
+    from the time the object is created until the object is deleted.
+
+    There are 4 storage durations:
+    1. automatic
+    2. static
+    3. allocated
+    4. thread
+
+*/
+
+int main(void) {
+}
