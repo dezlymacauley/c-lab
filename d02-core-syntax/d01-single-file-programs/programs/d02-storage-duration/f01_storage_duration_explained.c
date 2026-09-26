15a7f45458321ae8a8679a3ae5cb08f45e5875f6
dezlymacauley <dezlymacauley@gmail.com>
2026-09-26 09:21:31 +0200

    Refined knowledge

--- d02-core-syntax/d01-single-file-programs/programs/d02-storage-duration/f01_storage_duration_explained.c
+++ d02-core-syntax/d01-single-file-programs/programs/d02-storage-duration/f01_storage_duration_explained.c
@@ -1,15 +1,23 @@
 /*
     ABOUT: Storage Duration Explained
 
-    In C, storage duration refers to how long an object remains in memory,
-    from the time the object is created until the object is deleted.
+    In C, the term `storage duration` refers to how long a variable remains
+    valid for use.
 
-    There are 4 storage durations:
+    Storage duration looks at the following:
+    1. When is memory assigned to store the contents of that variable?
+    This memory is assigned by either the compiler or the operating system,
+    depending on how the variable is declared.
+
+    2. When is the memory assigned to that variable released back to the
+    program or the operating system?
+
+    The storage duration of a variable can be classified into one of these
+    categories:
     1. automatic
     2. static
     3. allocated
     4. thread
-
 */
 
 int main(void) {
